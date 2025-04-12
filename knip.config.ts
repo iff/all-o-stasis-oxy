f42b84b62f0646949af39aa15dd2eb0734616e9d
Tomas Carnecky <tomc@caurea.org>
2025-04-13 00:33:20 +0200

    Knip

---
+++ knip.config.ts
@@ -0,0 +1,30 @@
+import type { KnipConfig } from "knip";
+
+const config: KnipConfig = {
+  // Too many issues with these rules to have them enabled at this time
+  exclude: ["exports", "types", "duplicates"],
+
+  ignore: [
+    // These files are treated as static assets.
+    "static/**/*",
+  ],
+
+  ignoreDependencies: [
+    // next.config.js
+    "@babel/core",
+    "@catalog/loader",
+    "raw-loader",
+    "svg-react-loader",
+
+    // tslint.json
+    "tslint-config-prettier",
+
+    // vega
+    "react-vega",
+    "react-vega-lite",
+    "vega",
+    "vega-lite",
+  ],
+};
+
+export default config;
