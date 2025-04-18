47577c92ddfe825c15e381de69c5af01bc865ae0
Tomas Carnecky <tomc@caurea.org>
2025-04-18 22:15:22 +0200

    Fix bad pages/_document
    warn  - Your custom Document (pages/_document) did not render all the required subcomponent.
    
    Missing component: <Html />
    
    Read how to fix here: https://nextjs.org/docs/messages/missing-document-component

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, { Head, Main, NextScript } from "next/document";
+import Document, { Head, Html, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 import * as MUI from "@material-ui/core/styles";
 import React from "react";
@@ -31,7 +31,7 @@ export default class extends Document {
 
   render() {
     return (
-      <html>
+      <Html>
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
 
@@ -45,7 +45,7 @@ export default class extends Document {
           <Main />
           <NextScript />
         </body>
-      </html>
+      </Html>
     );
   }
 }
