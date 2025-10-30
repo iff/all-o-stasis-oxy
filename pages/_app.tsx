957ff6fd65bfcfbb3e09e57e0d9b8507f1da6035
Yves Ineichen <iff@yvesineichen.com>
2025-10-30 11:53:21 +0100

    mui theme if we need it (commented)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -52,6 +52,28 @@ const MyApp = ({ Component, pageProps, gymName }) => {
     };
   }, [app, setGenerationNumber]);
 
+  // const theme = {
+  //   shape: {
+  //     borderRadius: 4,
+  //   },
+  //
+  //   palette: {
+  //     primary: {
+  //       main: config.ThemeColorPrimary,
+  //     },
+  //     secondary: {
+  //       main: config.ThemeColorSecondary,
+  //     },
+  //   },
+  //
+  //   props: {
+  //     MuiButton: {
+  //       color: "secondary", // for blockchaefer
+  //       disableElevation: true,
+  //     },
+  //   },
+  // };
+
   return (
     <>
       <Head>
@@ -59,9 +81,11 @@ const MyApp = ({ Component, pageProps, gymName }) => {
         <style>{`:root { --theme-color-primary: ${config.ThemeColorPrimary}; --theme-text-primary: ${config.ThemeTextPrimary}; --theme-color-secondary: ${config.ThemeColorSecondary}; }`}</style>
       </Head>
 
+      {/* <ThemeProvider theme={theme}> */}
       <Env.Provider value={{ app: new App(app.data), config }}>
         <Component generationNumber={generationNumber} app={app} {...pageProps} />
       </Env.Provider>
+      {/* </ThemeProvider> */}
     </>
   );
 };
