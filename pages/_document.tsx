40cba2d5a1a821534cba19bb3fc5f725f1d87cb1
Yves Ineichen <iff@yvesineichen.com>
2026-04-26 15:59:02 +0200

    move assets to public

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -31,9 +31,9 @@ export default class extends Document {
     return (
       <Html>
         <Head>
-          <link rel="stylesheet" href="/static/app.css" />
-          <link rel="stylesheet" href="/static/rdp.css" />
-          <link rel="stylesheet" href="/static/UI-Icon-master/icon.css" />
+          <link rel="stylesheet" href="/public/app.css" />
+          <link rel="stylesheet" href="/public/rdp.css" />
+          <link rel="stylesheet" href="/public/UI-Icon-master/icon.css" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Advent+Pro" />
         </Head>
         <body>
